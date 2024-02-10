827520f32530fd7c74540bacb3362cad476d9ed9
unknown <dylan23smith1990@gmail.com>
2024-02-10 14:49:23 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RpgCharacter rpgCharacter = new RpgCharacter("gandalf", "wizard", "human");
+        }
+    }
+}
